ede4c44d0bd637351460c0f3356c8877710ea6b4
Vinusha <vinushanreddy98@gmail.com>
2021-01-10 20:06:14 +0530

    my fourth commit

---
+++ fbvProject2/myApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from myApp.models import Employee
+
+# Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+      l=['eno','ename','esal','eaddr']
+admin.site.register(Employee,EmployeeAdmin)
